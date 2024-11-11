b973bdbd362500b8c6ea476aad0c9251a363f30d
chrishoynck <94909944+chrishoynck@users.noreply.github.com>
2024-11-11 17:54:07 +0100

    merged mandelbrot_sim + worker (update worker)

--- worker.py
+++ worker.py
@@ -1,5 +1,20 @@
 import numpy as np
+
 # import matplotlib.pyplot as plt
+def mandelbrot_sampling(c_points, max_iter, escape_radius) -> tuple[int, int]:
+    number_outside = 0
+    total_numbers = len(c_points)
+    
+    for c in c_points:
+        z = 0
+        for iteration in range(max_iter):
+            z = z**2 + c
+            if abs(z) > escape_radius:
+                number_outside+=1
+                # If point escapes, break and count it as "outside"
+                break
+            
+    return total_numbers, number_outside
 
 
 def mandelbrot(c_points, max_iter, escape_radius) -> tuple[int, int]:
@@ -18,7 +33,7 @@ def mandelbrot(c_points, max_iter, escape_radius) -> tuple[int, int]:
             # take a gridpoint
             c = c_points[i, j]
             z = 0
-            for iteration in range(max_iter):
+            for _ in range(max_iter):
                 # update of z
                 z = z**2 + c
                 if abs(z) > escape_radius:
@@ -32,7 +47,6 @@ def mandelbrot(c_points, max_iter, escape_radius) -> tuple[int, int]:
     return (total_numbers, number_outside)
 
 
-
 def worker_function(xs):
     cpts, s = xs
     y = cpts.shape[0]
@@ -44,40 +58,8 @@ def worker_function(xs):
 def worker_function_sampling(pars):
     # add own sampling method instead of mandelbrot, and add sampling specific parameters to grid and bound. 
     # The function called (instead of mandelbrot) should essentially be the same as non-parallelized function. 
-    grid, grid_size, bound = pars 
-    total, out = mandelbrot(grid, bound, 2)
+    grid, grid_size, bound, run = pars
+    total, out = mandelbrot_sampling(grid, bound, 2)
 
     # note here, the actual grid is not returned, only the grid size
-    return (total, out), (grid_size, bound) 
-
-def random_points_generator(num_samples: int) -> np.ndarray:
-    """
-    Generate random points for the Monte Carlo Integration
-    """
-    real_parts = np.random.uniform(-2.0, 1.0, num_samples)
-    imag_parts = np.random.uniform(-1.5, 1.5, num_samples)
-    return real_parts + 1j * imag_parts
-
-def monte_carlo_calc(points: np.ndarray, max_iter: int) -> tuple[int, int]:
-    points_inside = 0
-    total_points = len(points)
-
-    for c in points:
-        z = 0
-        for _ in range(max_iter):
-            z = z*z + c
-            if abs(z) > 2.0:
-                break
-        else:
-            points_inside += 1
-
-    return total_points, points_inside
-    
-def worker_pure(pars):
-    """
-    Woker function for the Monte Carlo sampling
-    """
-    _, grid_size, max_iter = pars
-    points = random_points_generator(grid_size * grid_size)
-    total_points, points_inside = monte_carlo_calc(points, max_iter)
-    return (total_points, points_inside), (grid_size, max_iter)
+    return (total, out, run), (grid_size, bound) 
